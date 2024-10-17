865ed9d7975da54330d070d7c34500ccc07c3273
salvarys <123325469+salvarys@users.noreply.github.com>
2024-10-17 11:48:39 -0700

    Sky Update Mech.h

--- BattletechGame/BattletechGame/Mech.h
+++ BattletechGame/BattletechGame/Mech.h
@@ -1,1 +1,36 @@
 #pragma once
+#pragma once
+#include <iostream>
+#include <string>
+using std::cout;
+using std::endl;
+using std::string;
+using std::cin;
+
+class Mech {
+private:
+	int walk;
+	int heat;
+	int weight;
+	int jump; 
+public:
+	void fireWeapon() {
+
+	}
+	int walk() {
+
+		return walk;
+	}
+	void run() {
+
+	}
+	void jump() {
+
+	}
+	void melee() {
+
+	}
+
+};
+
+
